7a45c2ae3b69f24535c893f4d3d122b64145d853
moequan <maurice.s93@web.de>
2020-03-11 11:30:10 +0100

    Song polling with pusher.

---
+++ pusherpoll/config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI: 'mongodb://localhost:27017/HEYDJ',
+  pusherAppId: '961649',
+  pusherKey: '2747eb884c9bd801c2e9',
+  pusherSecret: 'e20d118b77a7abc986db',
+  pusherCluster: 'eu',
+  pusherEncrypted: true
+};
