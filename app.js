55f06651dd489c5ad8bd15c2df865c161fd7e3e5
moequan <maurice.s93@web.de>
2020-03-17 11:34:56 +0100

    Fixing POSTMAN issue.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
-let Post = require('./models/Post');
+
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,7 @@
  
 const express = require('express');
 const router = express.Router();
-let Post = require('./../models/Post');
 
-router.get('/', (req, res, next) => {
-  Post.find().exec((err, posts) => {
-      res.render('index', { posts: posts });
-  });
-
-});
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
