f8ecfe885f9dc7c01a3281fdce8dc979ad4c3f9b
moequan <maurice.s93@web.de>
2020-03-19 09:19:17 +0100

    Fixing eventcontroller.

--- routes/events.js
+++ routes/events.js
@@ -1,6 +1,29 @@
+// const express = require("express");
+// const router = express.Router();
+
+// const {
+//   getEvents,
+//   getEvent,
+//   addEvent,
+//   deleteEvent,
+//   updateEvent
+// } = require("../controllers/eventsController");
+
+// router
+//   .route("/")
+//   .get(getEvents)
+//   .post(addEvent);
+
+// router
+//   .post("/:id")
+//   .get(getEvent)
+//   .delete(deleteEvent)
+//   .put(updateEvent);
+
+// module.exports = router;
+
 const express = require("express");
 const router = express.Router();
-
 const {
   getEvents,
   getEvent,
@@ -8,16 +31,13 @@ const {
   deleteEvent,
   updateEvent
 } = require("../controllers/eventsController");
-
 router
   .route("/")
   .get(getEvents)
   .post(addEvent);
-
 router
-  .post("/:id")
+  .route("/:id")
   .get(getEvent)
   .delete(deleteEvent)
   .put(updateEvent);
-
 module.exports = router;
